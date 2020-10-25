60532b15c6628282a7d901f3926dea26ee784c0e
shopins <72446254+shopins@users.noreply.github.com>
2020-10-25 14:27:27 +1000

    25.10 14:27

--- SkillboxLesson14/CoreDataModel.swift
+++ SkillboxLesson14/CoreDataModel.swift
@@ -1,48 +1,34 @@
-//
-//  CoreDataModel.swift
-//  SkillboxLesson14
-//
-//  Created by Сергей Шопин on 25.10.2020.
-//
 
 import Foundation
 import CoreData
 import UIKit
 
 var toDoItemsCoreData : [NSManagedObject] = []
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+let managedContext = appDelegate.persistentContainer.viewContext
+
+func addItemCoreData(nameItem: String) {
+    let item = NSEntityDescription.insertNewObject(forEntityName: "ToDoItemCore", into: managedContext)
+    item.setValue(nameItem, forKey: "name")
+    item.setValue(false, forKey: "isComplited")
+    toDoItemsCoreData.append(item)
+    appDelegate.saveContext()
+}
 
 func changeStateCoreData(at index: Int) -> Bool {
-    
-    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return false}
     guard let state = toDoItemsCoreData[index].value(forKey: "isComplited") as? Bool else {return false}
-
     toDoItemsCoreData[index].setValue(!state, forKey: "isComplited")
     appDelegate.saveContext()
     return !state
 }
 
-func addItemCoreData(nameItem: String) {
-    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
-    let managedContext = appDelegate.persistentContainer.viewContext
-    let entity = NSEntityDescription.entity(forEntityName: "ToDoItemCore", in: managedContext)!
-    let item = NSManagedObject(entity: entity, insertInto: managedContext)
-    item.setValue(nameItem, forKey: "name")
-    item.setValue(false, forKey: "isComplited")
-    appDelegate.saveContext()
-    toDoItemsCoreData.append(item)
-}
-
 func removeItemCoreData(at index: Int) {
-    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
-    let managedContext = appDelegate.persistentContainer.viewContext
     managedContext.delete(toDoItemsCoreData[index])
-    appDelegate.saveContext()
     toDoItemsCoreData.remove(at: index)
+    appDelegate.saveContext()
 }
 
 func loadCoreData() {
-    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
-    let managedContext = appDelegate.persistentContainer.viewContext
     let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "ToDoItemCore")
     do {
         toDoItemsCoreData = try managedContext.fetch(fetchRequest)
